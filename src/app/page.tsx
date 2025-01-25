a9131deebaacd65e493bb82d67e464d6139aaa40
Hafeez Ur Rehman <hussainhafeez1100@gmail.com>
2025-01-25 13:50:19 +0530

    Fix: top margin on about me

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="h-full flex flex-col gap-4 justify-center items-center">
+      <main className="h-full flex flex-col gap-4 justify-center items-center mt-[24px]">
         <h1 className="text-3xl">Hafeez Ur Rehman</h1>
         <p>Full Stack Javascript Developer.</p>
         <h2>About Me</h2>
