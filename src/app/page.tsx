b9b42a7233eca23c6918d0856e90ee95a63a7750
Hafeez Ur Rehman <hussainhafeez1100@gmail.com>
2025-01-25 13:48:11 +0530

    Feat: added about me

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,28 @@ export default function Home() {
       <main className="h-full flex flex-col gap-4 justify-center items-center">
         <h1 className="text-3xl">Hafeez Ur Rehman</h1>
         <p>Full Stack Javascript Developer.</p>
+        <h2>About Me</h2>
+        <p>
+          Hello, World! I&apos;m Hafeez, a self-taught full-stack developer with
+          a passion for building innovative web and mobile applications.
+          I&apos;ve been coding since high school, and every day, I&apos;m
+          passionate about solving business challenges by combining technology
+          expertise with insights from business, marketing, and human
+          psychology. From my early days of self-study to my current role at
+          World Visa, I&apos;ve had the opportunity to work on a variety of
+          projects, from small-scale startups to large-scale enterprise
+          applications. I&apos;m proficient in a wide range of technologies,
+          including JavaScript, Python, Elixir, AI, and ML. I&apos;m always
+          looking for new challenges and opportunities to learn. I&apos;m
+          particularly interested in exploring AI and machine learning to build
+          smarter and more efficient applications. I&apos;m passionate about
+          reading books, collaborating with startups, exploring new hobbies,
+          playing chess, and diving into intellectual conversations over a good
+          cup of coffee. I also enjoy the challenge of applying programming
+          concepts to solve real-world problems—it&apos;s like debugging life,
+          one line at a time! Let&apos;s connect and build something amazing
+          together!
+        </p>
       </main>
     </div>
   );
